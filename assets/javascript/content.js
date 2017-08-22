cdd7ae64ede96eb07a8fff456d68ab4a3cad4ee7
Kristen Manning <kristenmanning16@gmail.com>
2017-08-22 18:43:44 -0400

    tons of css changes (no wifi on plane & bus...)

--- assets/javascript/content.js
+++ assets/javascript/content.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-
+  console.log("hi")
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD8J6gNDFLxiT-W3QgLpP83TQ6NJ5cWIeo",
@@ -10,29 +10,81 @@ $(document).ready(function() {
     storageBucket: "",
     messagingSenderId: "16195132803"
   };
+
   firebase.initializeApp(config);
 
-	var database = firebase.database();
+  var database = firebase.database();
+
+    // database.ref().on("value", function(snapshot) {
+    // $("#testing").html("")
+    // var masterSheet = snapshot.val();
+    // console.log(masterSheet)
+    // var data = masterSheet["masterSheet"][1]
+    // $("#testing").append(data)
+
+    // })
+
+    database.ref().on("value", function(snapshot) {
+        var headings = snapshot.val()["pageHeadings"];
+        for (h in headings[2]){
+          console.log(headings[2][h])
+          $(headings[2][h]).html("<h1>" + headings[1][h] + "</h1>")
+
+        }
 
 
-	 //  $(document).on("click", ".btn-info", function() {
+    })
 
-	 //  	window.location.replace($(this).data("href"));
+    database.ref().on("value", function(snapshot) {
+    $("#instructors").html("")
+    var instructors = snapshot.val()["instructors"];
+    console.log("instructors = ")
+    console.log(instructors)
 
-		// });
+    for (var i in instructors) {
+      if (instructors[i][0] && instructors[i][0] != "FullName"){
+        console.log(i)
+        console.log()
+        var newInstructor = $("<div class='col-sm-5 col-md-3 thumbnail'>")
+        var newInstructor_name = instructors[i][0]
+        var newInstructor_image = instructors[i][1]
+        var newInstructor_role = instructors[i][2]
+        var newInstructor_bios = [instructors[i][3], instructors[i][4], instructors[i][5], instructors[i][6], instructors[i][7]]
+        newInstructor.html("<img src='assets/images/" + newInstructor_image + "'><h3> " + newInstructor_name + " </h3> <h5>" + newInstructor_role + "</h5>")
+        for (b in newInstructor_bios){
+          if (newInstructor_bios[b]){
+            newInstructor.append("<p>" + newInstructor_bios[b] + "</p>")
+          }
+        }
+        console.log(newInstructor)
+        $("#instructors").append(newInstructor)
+        }
+      }
 
-	 //  $(document).on("click", "#LI", function() {
+    })
 
-	 //  	window.location.replace("https://www.linkedin.com/in/kristen-manning-83b266104/");
+    // var instructors = {0: {0: "Caley", 1: "caley.jpg", 2: "Manager", 3: "Caley Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. "}, 
+    //           1: {0: "Barbara", 1: "mrsg.jpg", 2: "Owner", 3: "Barbara Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. ",  4: "Barbara Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. ",  5: "Barbara Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. "}, 
+    //           2: {0: "Caley", 1: "caley.jpg", 2: "Manager", 3: "Caley Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. "}, 
+    //           3: {0: "Caley", 1: "caley.jpg", 2: "Manager", 3: "Caley Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here.  Lorem ipsum text here is what's here.  Lorem ipsum text here is what's here.  Lorem ipsum text here is what's here.  Lorem ipsum text here is what's here.  Lorem ipsum text here is what's here.  Lorem ipsum text here is what's here.  Lorem ipsum text here is what's here."}, 
+    //           4: {0: "Caley", 1: "caley.jpg", 2: "Manager", 3: "Caley Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. ", 4: "Lorem ipsum text here is what's here.  Lorem ipsum text here is what's here. "}, 
+    //           5: {0: "Barbara", 1: "mrsg.jpg", 2: "Owner", 3: "Barbara Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. ",  4: "Barbara Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. ",  5: "Barbara Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. ", 6: "Barbara Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. Lorem ipsum text here is what's here. "}, 
+    //         }
 
-		// });
 
-	 //  $(document).on("click", "#GH", function() {
 
-	 //  	window.location.replace("https://github.com/KristenManning");
+        //       <div class="thumbnail">
+        //     <img src="..." alt="...">
+        //     <div class="caption">
+        //       <h3>Thumbnail label</h3>
+        //       <p>...</p>
+        //       <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+        //     </div>
+        //   </div>
+        // </div>
 
-		// });
 });
+
 
 
 // Firebase example: 
